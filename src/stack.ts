473b5c7452181370b1a5d2ae8438a6cbf78d69ec
Aleksa Šuković <sukovic.aleksa@gmail.com>
2026-07-26 15:37:14 +0200

    fix: retain stack membership on destruction

--- src/stack.ts
+++ src/stack.ts
@@ -382,10 +382,7 @@ export class Stack {
             this.tab_disconnect(c);
             if (this.workspace === this.ext.active_workspace()) {
                 const win = this.ext.windows.get(c.entity);
-                if (win) {
-                    win.meta.get_compositor_private()?.show();
-                    win.stack = null;
-                }
+                if (win) win.meta.get_compositor_private()?.show();
             }
         }
 
